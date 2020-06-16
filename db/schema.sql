82e79552d59af94ee5f2379eb2e0b711b34bf515
Doggo <dhernandez24e@hotmail.com>
2020-06-15 18:15:49 -0700

    server and connection files populated

---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS tech_shop_db;
+
+CREATE DATABASE tech_shop_db;
---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+// const routes = require('./controllers')
+const sequelize = require('./config/connection');
+// const path = require('path');
+// const exphbs = require('express-handlebars');
+// const helpers = require('./utils/helpers');
+// const hbs = exphbs.create({ });
+// const session = require('express-session');
+
+// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+// const sess = {
+//     secret: 'N/A',
+//     cookie: {},
+//     resave: false,
+//     saveUninitialized: true,
+//     store: new SequelizeStore({
+//         db: sequelize
+//     })
+// };
+
+const app = express();
+const PORT = process.env.PORT || 3002;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, 'public')));
+// app.use(session(sess));
+// app.use(routes);
+
+// app.engine('handlebars', hbs.engine);
+// app.set('view engine', 'handlebars');
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Now Listening to PORT ${PORT}`));
+});
