602e5eb2dd02d0aac1e3895b5bb15f73381b024b
Doggo <dhernandez24e@hotmail.com>
2020-06-21 17:05:11 -0700

    add profit routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const userRoutes = require('./user-routes');
 const productRoutes = require('./product-routes');
 const categoryRoutes = require('./category-routes');
+const profitRoutes = require('./product_profit-routes');
 
 router.use('/users', userRoutes);
 router.use('/products', productRoutes);
 router.use('/categories', categoryRoutes);
+router.use('/profits', profitRoutes);
 
 module.exports = router
