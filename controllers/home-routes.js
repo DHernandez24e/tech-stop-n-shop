1f8c9f4479ba4691f797decf7fa98448aadcd747
Doggo <dhernandez24e@hotmail.com>
2020-06-22 20:10:48 -0700

    add files from homepage branch

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,193 +1,58 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Product, Category, Product_Profit } = require('../models');
-// const { in } = require('sequelize/types/lib/operators');
-// const {
-//     Post,
-//     User,
-//     Comment
-// } = require('../models');
+const {
+    Product,
+    User,
+    Comment
+} = require('../models');
 
-router.get('/', (req, res) => {
-    console.log(req.session);
-    res.render('homepage', {});
-  }); 
-
-// router.get('/profit', (req, res) => {
+// router.get('/', (req, res) => {
 //     console.log(req.session);
-//     const hello = "hello there"
-//     res.render('profit', {hello:hello});
+//     res.render('homepage', {});
 //   }); 
 
-//GET all products
-router.get('/profit', (req, res) => {
-    Category.findAll({
-        attributes: ['id', 'category_name'],
-        include: 
-        // [
-        {
-            model: Product,
-            attributes: ['product_name', 'price', 'stock'],
-            include: {
-                model: Product_Profit,
-                attributes: ['num_sold','cost']
-              }
-        },
-
-    })
-  .then(dbPostData => {
-    // serialize data before passing to template
-    // const posts = dbPostData;  
-    // console.log(dbPostData);
-    let test2 = JSON.stringify(dbPostData);
-    let parsePost =JSON.parse(JSON.stringify(dbPostData));
-    console.log(parsePost);
-    // let test1 = "hi";
-    // let test2 = "hey";
-    // console.log(parsePost[0].product_profits[0].num_sold);
-    let test = parsePost[1].products[0].product_profits[0].num_sold;
-    let lengthTest = parsePost.length;
-    console.log("lengthTest is :");
-    console.log(test)
-    var soldTimesCost = 0
-    var inventTimesCost = 0
-    var soldTimesPrice = 0
-    // for (let i = 0; i < parsePost.length; i++) {
-    //     sumSold[i] = 0;
-    //     sumInvent[i] = 0;
-    //     // console.log(sumSold[i])
-    //     // console.log(sumInvent[i])
-    // }
-
-    for (let i = 0; i < parsePost.length; i++) {
-
-        for (let j = 0; j < parsePost[i].products.length; j++) {
-        soldTimesCost = soldTimesCost + parsePost[i].products[j].product_profits[0].num_sold*parsePost[i].products[j].product_profits[0].cost;
-        inventTimesCost = inventTimesCost + parsePost[i].products[j].stock*parsePost[i].products[j].product_profits[0].cost;
-        soldTimesPrice = soldTimesPrice + parsePost[i].products[j].product_profits[0].num_sold*parsePost[i].products[j].price;
-
-        // console.log(parsePost[i].products[j].product_profits[0].num_sold)
-        // console.log(parsePost[i].products[j].stock)
-        // console.log(sumSold[i])
-        // console.log(sumInvent[i])
-        }
-
-    }
-
-    let debtTotal = soldTimesCost + inventTimesCost
-    let incomeTotal = soldTimesPrice;
-    let profitTotal = incomeTotal - debtTotal;
-    if (profitTotal > 0){
-        var profitFlag = true;
-    } else {
-        var profitFlag = false;
-    }
-    console.log(debtTotal)
-    console.log(incomeTotal)
-
-
-    // var arraySumSold = []
-    // var arraySumInvent = []
-    // for (let i = 0; i < parsePost.length; i++) {
-    //     console.log(sumSold[i])
-    //     console.log(sumInvent[i])
-    //     arraySumSold.push(sumSold[i])
-    //     arraySumInvent.push(sumInvent[i])
-    // }
-
-    
-    // let objSumSold = []
-    // let objSumInvent = []
-    // console.log (arraySumSold)
-    // console.log(arraySumInvent)
-    // parsePost = parsePost.push(objSumSold)
-    // parsePost = parsePost.push(objSumInvent)
-
-    // parsePost['objSumSold'].push(arraySumSold)
-    // parsePost['objSumInvent'].push(arraySumInvent)
-    console.log(parsePost)
-
-    // console.log("online 34", JSON.stringify(dbPostData));
-    // const hi = new dbPostData[0].product
-    // console.log(hi);
-    res.render('profit', {parsePost, profitTotal, profitFlag, test2});
-    // const posts = "hello";
-    // res.render('profit', {posts: "hello"});
-  })
-  .catch(err => res.status(500).json(err));
-});
-
-router.get('/product-inventory', (req, res) => {
+router.get('/', (req, res) => {
     Product.findAll({
-        attributes: ['id', 'product_name'],
-        // include: 
-        // [
-        // {
-        //     model: Product,
-        //     attributes: ['product_name', 'price', 'stock'],
-        //     include: {
-        //         model: Product_Profit,
-        //         attributes: ['num_sold','cost']
-        //       }
-        // },
-
-    })
-  .then(dbPostData => {
-    let parsePost =JSON.parse(JSON.stringify(dbPostData));
-    res.render('product-inventory', {parsePost});
-    // const posts = "hello";
-    // res.render('profit', {posts: "hello"});
-  })
-  .catch(err => res.status(500).json(err));
-});
-
-router.get('/products-update/:id', (req, res) => {
-Product.findOne({
-    
         where: {
-          id: req.params.id
-        }
-    })  .then(dbUserData => {
-        // if (!dbUserData) {
-        //   res.status(404).json({ message: 'No user found with this id' });
-        //   return;
-        // }
-        res.render("products-update");
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-    });
-
-// router.put('/products-update/:id', (req, res) => {
-//   Product.update(
-//     {
-//       title: req.body.title
-//     },
-//     {
-//       where: {
-//         id: req.params.id
-//       }
-//     }
-//   )
-//     .then(dbPostData => {
-//       if (!dbPostData) {
-//         res.status(404).json({ message: 'No post found with this id' });
-//         return;
-//       }
-//       res.render('products-update');
-//     })
-//     .catch(err => {
-//       console.log(err);
-//       res.status(500).json(err);
-//     });
-// });
-
-router.get('/update-inventory', (req, res) => {
- res.render('update-inventory')
-})
-
+            featured: true
+        },
+            attributes: [
+                'id',
+                'product_name',
+                'price',
+                'stock'
+            ],
+            // include: [
+            //     /*{
+            //       model: Comment,
+            //       attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            //       include: {
+            //         model: User,
+            //         attributes: ['username']
+            //       }
+            //     },*/
+            //     {
+            //         //  model: Product
+            //         // attributes: ['username']
+            //     }
+            // ]
+        })
+        .then(dbPostData => {
+            const products = dbPostData.map(products => products.get({
+                plain: true
+            }));
+            // pass a single post object into the homepage template
+            //res.render('homepage', { posts });
+            res.render('homepage', {
+                products
+                ,loggedIn: req.session.loggedIn
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
 router.get('/login', (req, res) => {
     if (req.session.loggedIn) {
--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes.js');
 
+ 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+
 
 router.use((req, res) => {
   res.status(404).end();
