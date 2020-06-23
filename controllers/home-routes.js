64b5303c79c3c79d077e44f0da0f9213bad7ad21
Joshua Hartshorn <Pops@Joshuas-MacBook-Air-2.local>
2020-06-22 18:48:31 -0700

    Updated homepage and login

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,56 +1,58 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-// const {
-//     Post,
-//     User,
-//     Comment
-// } = require('../models');
-
-router.get('/', (req, res) => {
-    console.log(req.session);
-    res.render('homepage', {});
-  }); 
+const {
+    Product,
+    User,
+    Comment
+} = require('../models');
 
 // router.get('/', (req, res) => {
-//     Post.findAll({
-//             attributes: [
-//                 'id',
-//                 'post_url',
-//                 'title',
-//                 'created_at'
-//                 //,[sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-//             ],
-//             include: [
-//                 /*{
-//                   model: Comment,
-//                   attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-//                   include: {
-//                     model: User,
-//                     attributes: ['username']
-//                   }
-//                 },*/
-//                 {
-//                     model: User,
-//                     attributes: ['username']
-//                 }
-//             ]
-//         })
-//         .then(dbPostData => {
-//             const posts = dbPostData.map(post => post.get({
-//                 plain: true
-//             }));
-//             // pass a single post object into the homepage template
-//             //res.render('homepage', { posts });
-//             res.render('homepage', {
-//                 posts,
-//                 loggedIn: req.session.loggedIn
-//             });
-//         })
-//         .catch(err => {
-//             console.log(err);
-//             res.status(500).json(err);
-//         });
-// });
+//     console.log(req.session);
+//     res.render('homepage', {});
+//   }); 
+
+router.get('/', (req, res) => {
+    Product.findAll({
+        where: {
+            featured: true
+        },
+            attributes: [
+                'id',
+                'product_name',
+                'price',
+                'stock'
+            ],
+            // include: [
+            //     /*{
+            //       model: Comment,
+            //       attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            //       include: {
+            //         model: User,
+            //         attributes: ['username']
+            //       }
+            //     },*/
+            //     {
+            //         //  model: Product
+            //         // attributes: ['username']
+            //     }
+            // ]
+        })
+        .then(dbPostData => {
+            const products = dbPostData.map(products => products.get({
+                plain: true
+            }));
+            // pass a single post object into the homepage template
+            //res.render('homepage', { posts });
+            res.render('homepage', {
+                products
+                ,loggedIn: req.session.loggedIn
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
 router.get('/login', (req, res) => {
     if (req.session.loggedIn) {
