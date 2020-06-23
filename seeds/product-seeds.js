465d535a6c2228b4f0ef7e0038805bfe981fac72
Doggo <dhernandez24e@hotmail.com>
2020-06-22 20:26:25 -0700

    re-seeded db

--- seeds/product-seeds.js
+++ seeds/product-seeds.js
@@ -5,31 +5,36 @@ const productData = [
     product_name: 'Samsung Galaxy Cell Phone',
     price: 398.0,
     stock: 5,
-    category_id: 3
+    category_id: 3,
+    featured: true
   },
   {
     product_name: 'Magnavox Flat Screen TV',
     price: 280.0,
     stock: 7,
-    category_id: 1
+    category_id: 1,
+    featured: true
   },
   {
     product_name: 'Lenovo Laptop',
     price: 890.00,
     stock: 3,
-    category_id: 2
+    category_id: 2,
+    featured: true
   },
   {
     product_name: 'I Phone',
     price: 589.0,
     stock: 2,
-    category_id: 3
+    category_id: 3,
+    featured: false
   },
   {
     product_name: 'HP Laptop',
     price: 650.0,
     stock: 5,
-    category_id: 2
+    category_id: 2,
+    featured: false
   },
 ];
 
--- server.js
+++ server.js
@@ -5,19 +5,19 @@ const path = require('path');
 const exphbs = require('express-handlebars');
 // const helpers = require('./utils/helpers');
 const hbs = exphbs.create({ });
-// const session = require('express-session');
+const session = require('express-session');
 
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-// const sess = {
-//     secret: 'N/A',
-//     cookie: {},
-//     resave: false,
-//     saveUninitialized: true,
-//     store: new SequelizeStore({
-//         db: sequelize
-//     })
-// };
+const sess = {
+    secret: 'N/A',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -25,7 +25,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use(session(sess));
+app.use(session(sess));
 app.use(routes);
 
 app.engine('handlebars', hbs.engine);
