1f4c4d04e8e86679cac18893b3450f1ae617106d
Doggo <dhernandez24e@hotmail.com>
2020-06-23 21:06:17 -0700

    fixed issues with page, implemented features

--- public/javascript/homepage.js
+++ public/javascript/homepage.js
@@ -165,7 +165,7 @@ var shoppingCart = (function() {
         + "<td><div class='input-group'><button class='minus-item input-group-addon btn btn-primary' data-name= '" + cartArray[i].name + "'>-</button>"
         + "<input type='number' class='item-count form-control' data-name='" + cartArray[i].name + "' value='" + cartArray[i].count + "'>"
         + "<button class='plus-item btn btn-primary input-group-addon' data-name= '" + cartArray[i].name + "'>+</button></div></td>"
-        + "<td><button class='delete-item btn btn-danger' data-name=" + cartArray[i].name + ">X</button></td>"
+        + "<td><button class='delete-item btn btn-danger' data-name='" + cartArray[i].name + "'>X</button></td>"
         + " = " 
         + "<td>" + cartArray[i].total + "</td>" 
         +  "</tr>";
