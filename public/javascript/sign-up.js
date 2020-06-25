6019f69cebc3fc33efb4fa2666aa189b34150c27
Aaron Lisker <aelisker@gmail.com>
2020-06-24 23:45:17 -0700

    added search feature

--- public/javascript/sign-up.js
+++ public/javascript/sign-up.js
@@ -1,39 +1,49 @@
 async function signupFormHandler(event) {
-    event.preventDefault();
-    // const id = window.location.toString().split('/')[
-    //     window.location.toString().split('/').length - 1
-    // ];
-    const username = document.querySelector('input[name="username"]').value.trim();
-    const email = document.querySelector('input[name="email"]').value.trim();
-    const password = document.querySelector('input[name="password"]').value.trim();
-    console.log(" I am here")
-    // console.log(userName)
-    console.log(email)
-    console.log(password)
-    // console.log(stock)
+  event.preventDefault();
+  // const id = window.location.toString().split('/')[
+  //     window.location.toString().split('/').length - 1
+  // ];
+  const username = document.querySelector('input[name="username"]').value.trim();
+  const email = document.querySelector('input[name="email"]').value.trim();
+  const password = document.querySelector('input[name="password"]').value.trim();
+  console.log(" I am here")
+  // console.log(userName)
+  console.log(email)
+  console.log(password)
+  // console.log(stock)
 
-    if (username && password && email) {
-      const response = await fetch(`/api/users`, {
-          method: 'POST',
-          body: JSON.stringify({
-            username,
-            email,
-            password
-          }),
-          headers: {
-            'Content-Type': 'application/json'
-          }
-        });
-        // console.log(" I am here")
-        // document.location.replace('/');
-
-          // check response status
-      if (response.ok) {
-        console.log('success');
-        document.location.replace('/');
-      } else {
-        alert(response.statusText);
+  if (username && password && email) {
+    const response = await fetch(`/api/users`, {
+      method: 'POST',
+      body: JSON.stringify({
+        username,
+        email,
+        password
+      }),
+      headers: {
+        'Content-Type': 'application/json'
       }
+    });
+    // console.log(" I am here")
+    // document.location.replace('/');
+
+    // check response status
+    if (response.ok) {
+      const res2 = await fetch('/api/users/login', {
+        method: 'POST',
+        body: JSON.stringify({
+          username,
+          password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+      if (res2.ok) {
+        document.location.replace('/');
+      }
+    } else {
+      alert(response.statusText)
+    }
+
   }
 };
 
